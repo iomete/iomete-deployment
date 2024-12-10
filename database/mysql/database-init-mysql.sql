48344f43a2c24df3593fa4cbba2e76f9e9624e41
Fuad Musayev <fuadmusayev_@outlook.com>
2024-12-10 06:08:31 +0400

    Deleted old databases

--- database/mysql/database-init-mysql.sql
+++ database/mysql/database-init-mysql.sql
@@ -4,8 +4,6 @@ CREATE DATABASE IF NOT EXISTS iomete_iam_db;
 CREATE DATABASE IF NOT EXISTS iomete_sql_db;
 CREATE DATABASE IF NOT EXISTS iomete_iceberg_db;
 CREATE DATABASE IF NOT EXISTS iomete_metastore_db;
-CREATE DATABASE IF NOT EXISTS iomete_keycloak_db;
-CREATE DATABASE IF NOT EXISTS iomete_ranger_db;
 CREATE DATABASE IF NOT EXISTS iomete_catalog_db;
 
 -- Create User
@@ -17,6 +15,4 @@ GRANT ALL ON iomete_iam_db.* TO iomete_user;
 GRANT ALL ON iomete_sql_db.* TO iomete_user;
 GRANT ALL ON iomete_iceberg_db.* TO iomete_user;
 GRANT ALL ON iomete_metastore_db.* TO iomete_user;
-GRANT ALL ON iomete_keycloak_db.* TO iomete_user;
-GRANT ALL ON iomete_ranger_db.* TO iomete_user;
 GRANT ALL ON iomete_catalog_db.* TO iomete_user;
--- database/postgresql/database-init-postgres.sql
+++ database/postgresql/database-init-postgres.sql
@@ -4,8 +4,6 @@ CREATE DATABASE iomete_iam_db;
 CREATE DATABASE iomete_sql_db;
 CREATE DATABASE iomete_iceberg_db;
 CREATE DATABASE iomete_metastore_db;
-CREATE DATABASE iomete_keycloak_db;
-CREATE DATABASE iomete_ranger_db;
 CREATE DATABASE iomete_catalog_db;
 
 -- Create User
@@ -17,8 +15,6 @@ GRANT ALL PRIVILEGES ON DATABASE iomete_iam_db TO iomete_user;
 GRANT ALL PRIVILEGES ON DATABASE iomete_sql_db TO iomete_user;
 GRANT ALL PRIVILEGES ON DATABASE iomete_iceberg_db TO iomete_user;
 GRANT ALL PRIVILEGES ON DATABASE iomete_metastore_db TO iomete_user;
-GRANT ALL PRIVILEGES ON DATABASE iomete_keycloak_db TO iomete_user;
-GRANT ALL PRIVILEGES ON DATABASE iomete_ranger_db TO iomete_user;
 GRANT ALL PRIVILEGES ON DATABASE iomete_catalog_db TO iomete_user;
 
 -- Switch to created databases and grant privileges on public schema to the user (repeat for each database)
